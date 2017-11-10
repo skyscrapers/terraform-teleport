612410fec7db91ba8840a133637ff9a008d8f733
iuri aranda <iuri.aranda@gmail.com>
2017-11-10 15:28:25 +0100

    Forgot to change the outputs for my last commit

--- teleport-security-groups/outputs.tf
+++ teleport-security-groups/outputs.tf
@@ -1,9 +1,5 @@
-output "auth_sg_id" {
+output "bastion_sg_id" {
   value = "${aws_security_group.teleport_auth.id}"
-}
-
-output "proxy_sg_id" {
-  value = "${aws_security_group.teleport_proxy.id}"
 }
 
 output "node_sg_id" {
