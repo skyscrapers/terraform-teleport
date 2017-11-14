714604440d81148103b253da8bfc248ddcb966c7
iuri aranda <iuri.aranda@gmail.com>
2017-11-14 15:18:33 +0100

    Added missing rule for trusted clusters reverse ssh tunnel

--- teleport-security-groups/bastion.tf
+++ teleport-security-groups/bastion.tf
@@ -60,11 +60,19 @@ resource "aws_security_group_rule" "teleport_https_proxy_from_world" {
   security_group_id = "${aws_security_group.teleport_bastion.id}"
 }
 
-# This is needed for the trusted clusters feature, the auth server needs to connect to upstream Teleport clusters
+# These are needed for the trusted clusters feature, the auth server needs to connect to upstream Teleport clusters
 resource "aws_security_group_rule" "teleport_https_auth_to_world" {
   type              = "egress"
   from_port         = 3080
   to_port           = 3080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.teleport_bastion.id}"
+}
+resource "aws_security_group_rule" "teleport_reverse_ssh_proxy_to_world" {
+  type              = "egress"
+  from_port         = 3024
+  to_port           = 3024
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.teleport_bastion.id}"
