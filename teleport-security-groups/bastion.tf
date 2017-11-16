57291a8b9efc5d985ae6098cbfe1392a2b069060
iuri aranda <iuri.aranda@gmail.com>
2017-11-16 17:30:29 +0100

    Allow 3025 ingress from everywhere in bastion sg

--- teleport-security-groups/bastion.tf
+++ teleport-security-groups/bastion.tf
@@ -6,12 +6,12 @@ resource "aws_security_group" "teleport_bastion" {
 
 ########## Auth related rules
 resource "aws_security_group_rule" "teleport_auth_from_nodes" {
-  type                     = "ingress"
-  from_port                = 3025
-  to_port                  = 3025
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.teleport_node.id}"
-  security_group_id        = "${aws_security_group.teleport_bastion.id}"
+  type              = "ingress"
+  from_port         = 3025
+  to_port           = 3025
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Right now the nodes access the auth server through its public IP address. TODO change it back when they can use the private one
+  security_group_id = "${aws_security_group.teleport_bastion.id}"
 }
 
 resource "aws_security_group_rule" "teleport_auth_from_proxy_self" {
