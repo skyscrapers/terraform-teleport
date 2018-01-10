8de4e6cf8b1a941d0acc67dd695d742506713b8d
Mattias Gees <mattias.gees@gmail.com>
2018-01-10 17:03:58 +0100

    Add tsh entry

--- teleport-ecs/dns.tf
+++ teleport-ecs/dns.tf
@@ -13,6 +13,11 @@ data "aws_lb" "alb" {
   arn   = "${data.aws_lb_listener.alb.load_balancer_arn}"
 }
 
+data "aws_lb" "nlb" {
+  count = "${var.create_dns_record ? 1 : 0}"
+  arn   = "${var.nlb_arn}"
+}
+
 resource "aws_route53_record" "teleport" {
   count   = "${var.create_dns_record ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.teleport.zone_id}"
@@ -22,6 +27,19 @@ resource "aws_route53_record" "teleport" {
   alias {
     name                   = "${data.aws_lb.alb.dns_name}"
     zone_id                = "${data.aws_lb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "teleport" {
+  count   = "${var.create_dns_record ? 1 : 0}"
+  zone_id = "${data.aws_route53_zone.teleport.zone_id}"
+  name    = "tsh.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${data.aws_lb.nlb.dns_name}"
+    zone_id                = "${data.aws_lb.nlb.zone_id}"
     evaluate_target_health = true
   }
 }
