070179e76d4ce6d877e00ee0601cf849859e35c4
iuri aranda <iuri.aranda@gmail.com>
2018-03-01 19:17:40 +0100

    Remove obsolete modules

--- teleport-security-groups/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID where to deploy the security groups"
-}
-
-variable "cidr_blocks" {
-  description = "CIDR blocks from where to allow connections to the Teleport cluster."
-  default     = ["0.0.0.0/0"]
-}
