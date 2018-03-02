18047a798adc14f012d07d0ac4f6f5f05e2c792b
iuri aranda <iuri.aranda@gmail.com>
2018-03-02 12:40:28 +0100

    Fix teleport-server variable descriptions

--- teleport-server/variables.tf
+++ teleport-server/variables.tf
@@ -23,86 +23,86 @@ variable "r53_zone" {
 }
 
 variable "teleport_cluster_name" {
-  description = "DNS subdomain that will be created for the teleport server."
-  default     = ""
-}
-
-variable "teleport_dynamodb_table" {
   description = "Name of the teleport cluster."
   default     = ""
 }
 
-variable "instance_type" {
+variable "teleport_dynamodb_table" {
   description = "Name of the DynamoDB table to configure in Teleport."
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance."
   default     = "t2.small"
 }
 
 variable "letsencrypt_email" {
-  description = "Instance type for the EC2 instance."
+  description = "Email to use to register to letsencrypt."
   default     = "letsencrypt@skyscrapers.eu"
 }
 
 variable "teleport_log_output" {
-  description = "Email to use to register to letsencrypt."
+  description = "Teleport logging configuration, possible values are `stdout`, `stderr` and `syslog`."
   default     = "stdout"
 }
 
 variable "teleport_log_severity" {
-  description = "Teleport logging configuration, possible values are `stdout`, `stderr` and `syslog`."
+  description = "Teleport logging configuration, possible severity values are `INFO`, `WARN` and `ERROR`."
   default     = "ERROR"
 }
 
 variable "teleport_auth_tokens" {
-  description = "Teleport logging configuration, possible severity values are `INFO`, `WARN` and `ERROR`."
+  description = "List of static tokens to configure in the Teleport server. See the official documentation on static tokens [here](https://gravitational.com/teleport/docs/2.3/admin-guide/#static-tokens)."
   default     = []
 }
 
 variable "teleport_session_recording" {
-  description = "List of static tokens to configure in the Teleport server. See the official documentation on static tokens [here](https://gravitational.com/teleport/docs/2.3/admin-guide/#static-tokens)."
+  description = "Setting for configuring session recording in Teleport. Check the [official documentation](https://gravitational.com/teleport/docs/2.4/admin-guide/#configuration) for more info."
   default     = "node"
 }
 
 variable "allowed_node_cidr_blocks" {
-  description = "Setting for configuring session recording in Teleport. Check the [official documentation](https://gravitational.com/teleport/docs/2.4/admin-guide/#configuration) for more info."
+  description = "CIDR blocks that are allowed to access the API interface in the `auth` server."
   default     = ["10.0.0.0/8"]
 }
 
 variable "allowed_cli_cidr_blocks" {
-  description = "CIDR blocks that are allowed to access the API interface in the `auth` server."
-  default     = ["0.0.0.0/0"]
-}
-
-variable "allowed_web_cidr_blocks" {
   description = "CIDR blocks that are allowed to access the cli interface of the `proxy` server."
   default     = ["0.0.0.0/0"]
 }
 
-variable "allowed_tunnel_cidr_blocks" {
+variable "allowed_web_cidr_blocks" {
   description = "CIDR blocks that are allowed to access the web interface of the `proxy` server."
   default     = ["0.0.0.0/0"]
 }
 
-variable "root_vl_type" {
+variable "allowed_tunnel_cidr_blocks" {
   description = "CIDR blocks that are allowed to access the reverse tunnel interface of the `proxy` server."
+  default     = ["0.0.0.0/0"]
+}
+
+variable "root_vl_type" {
+  description = "Volume type for the root volume of the EC2 instance. Can be `standard`, `gp2`, or `io1`."
   default     = "gp2"
 }
 
 variable "root_vl_size" {
-  description = "Volume type for the root volume of the EC2 instance. Can be `standard`, `gp2`, or `io1`."
+  description = "Volume size for the root volume of the EC2 instance, in gigabytes."
   default     = 16
 }
 
 variable "root_vl_delete" {
-  description = "Volume size for the root volume of the EC2 instance, in gigabytes."
+  description = "Whether the root volume of the EC2 instance should be destroyed on instance termination."
   default     = "true"
 }
 
 variable "acme_server" {
-  description = "Whether the root volume of the EC2 instance should be destroyed on instance termination."
+  description = "ACME server where to point `certbot` on the Teleport server to fetch an SSL certificate. Useful if you want to point to the letsencrypt staging server."
   default     = "https://acme-v01.api.letsencrypt.org/directory"
 }
 
 variable "teleport_subdomain" {
-  description = "ACME server where to point `certbot` on the Teleport server to fetch an SSL certificate. Useful if you want to point to the letsencrypt staging server."
+  description = "DNS subdomain that will be created for the teleport server."
   default     = "teleport"
 }
