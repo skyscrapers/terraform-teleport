180e7c3f5fd7d5dffbb8df5f01098c92be28d656
iuri aranda <iuri.aranda@gmail.com>
2018-03-05 16:03:58 +0100

    Remove obsolete modules (#10)

--- teleport-ecs/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "teleport" {
-  name              = "teleport_logs_${var.environment}_${var.project}"
-  retention_in_days = "7"
-
-  tags {
-    Environment = "${var.environment}"
-  }
-}
--- teleport-security-groups/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_sg_id" {
-  value = "${aws_security_group.teleport_bastion.id}"
-}
-
-output "node_sg_id" {
-  value = "${aws_security_group.teleport_node.id}"
-}
