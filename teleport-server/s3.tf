f71059ac7b8a69ca5e1a8c23b3f0c7ddec4677cc
Philip Dubois <hello@philipdubois.be>
2020-02-21 13:27:18 +0100

    Block public S3 access completely

--- teleport-server/s3.tf
+++ teleport-server/s3.tf
@@ -7,3 +7,11 @@ resource "aws_s3_bucket" "sessions" {
     Environment = var.environment
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "sessions" {
+  bucket                  = aws_s3_bucket.sessions.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
