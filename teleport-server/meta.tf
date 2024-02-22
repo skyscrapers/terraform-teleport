dd3990b5d1d7b83fada3fdbc9f1b4a4a85ecdb2e
Philip Dubois <hello@philipdubois.be>
2024-02-22 14:51:04 +0100

    Allow updating R53 via different provider (#44)

--- teleport-server/meta.tf
+++ teleport-server/meta.tf
@@ -1,14 +1,10 @@
 locals {
-  teleport_domain_name    = "${var.teleport_subdomain}.${var.r53_zone}"
+  teleport_domain_name    = "${var.teleport_hostname}.${var.r53_zone}"
   teleport_dynamodb_table = coalesce(var.teleport_dynamodb_table, local.teleport_domain_name)
   teleport_cluster_name   = coalesce(var.teleport_cluster_name, local.teleport_domain_name)
 }
 
 data "aws_region" "current" {
-}
-
-data "aws_route53_zone" "root" {
-  name = var.r53_zone
 }
 
 data "aws_subnet" "teleport" {
--- teleport-server/versions.tf
+++ teleport-server/versions.tf
@@ -1,4 +1,11 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.route53]
+      version               = ">= 4.0"
+    }
+  }
 }
